0d514659fdbca2bc6cbe9831c542dcce699a715f
Johnny Nguyen <jnguyen1236@gmail.com>
2018-01-23 22:03:17 -0500

    Added the default seed command into the json config, and config setup swift file

--- Sources/App/Setup/Config+Setup.swift
+++ Sources/App/Setup/Config+Setup.swift
@@ -2,25 +2,31 @@ import FluentProvider
 import PostgreSQLProvider
 
 extension Config {
-    public func setup() throws {
-        // allow fuzzy conversions for these types
-        // (add your own types here)
-        Node.fuzzy = [Row.self, JSON.self, Node.self]
-
-        try setupProviders()
-        try setupPreparations()
-    }
+  public func setup() throws {
+    // allow fuzzy conversions for these types
+    // (add your own types here)
+    Node.fuzzy = [Row.self, JSON.self, Node.self]
     
-    /// Configure providers
-    private func setupProviders() throws {
-        try addProvider(FluentProvider.Provider.self)
-        try addProvider(PostgreSQLProvider.Provider.self)
-    }
-    
-    /// Add all models that should have their
-    /// schemas prepared before the app boots
-    private func setupPreparations() throws {
-      preparations.append(Role.self)
-      preparations.append(User.self)
-    }
+    try setupProviders()
+    try setupPreparations()
+    setupCommands()
+  }
+  
+  /// Configure providers
+  private func setupProviders() throws {
+    try addProvider(FluentProvider.Provider.self)
+    try addProvider(PostgreSQLProvider.Provider.self)
+  }
+  
+  /// configures our custom commands that we have
+  private func setupCommands() {
+    addConfigurable(command: SeedCommand.init, name: "seed")
+  }
+  
+  /// Add all models that should have their
+  /// schemas prepared before the app boots
+  private func setupPreparations() throws {
+    preparations.append(Role.self)
+    preparations.append(User.self)
+  }
 }
