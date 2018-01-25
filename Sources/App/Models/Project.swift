507262409c353a277510fa52ff76415fefb5bc71
Liam Goodwin <liam.goodwin1@stclairconnect.ca>
2018-01-24 20:42:47 -0500

    Added the Project model, and entered the variables and initializer

---
+++ Sources/App/Models/Project.swift
@@ -0,0 +1,44 @@
+//
+//  Project.swift
+//  App
+//
+//  Created by Liam Goodwin on 2018-01-24.
+//
+
+import Foundation
+import Vapor
+import FluentProvider
+
+final class Project: Model, Timestampable {
+  var storage: Storage
+
+  //MARK: Project Table Variables
+  var user_id: Int
+  var name: String
+  var category_id: Identifier
+  var role_id: Identifier
+  var project_description: String
+  var description_needs: String
+
+  //MARK: Initialize Project Table
+  init(user_id: Int, name: String, category_id: Identifier, role_id: Identifier, project_description: String, description_needs: String) {
+    self.user_id = user_id
+    self.name = name
+    self.category_id = category_id
+    self.role_id = role_id
+    self.project_description = project_description
+    self.description_needs = description_needs
+  }
+  
+  //MARK: Initialize Row
+  init(row: Row) throws {
+    
+  }
+  
+  
+  func makeRow() throws -> Row {
+    <#code#>
+  }
+  
+  
+}
