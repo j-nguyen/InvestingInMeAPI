335139c1d70372390e153d71fbb1db6fd1361b8e
Jarrod Maeckeler <jarrodmaeckeler@gmail.com>
2018-01-25 12:59:14 -0500

    Setup get method for Connection

--- Sources/App/Controllers/ConnectionController.swift
+++ Sources/App/Controllers/ConnectionController.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import Vapor
+import HTTP
+
+final class ConnectionController {
+  
+  //MARK: Show Connection Invite
+  func show(_ request: Request) throws -> ResponseRepresentable {
+    
+    //Declare the user_id requested in the url
+    guard let id = request.parameters["id"]?.int
+      else {
+        throw Abort.badRequest
+    }
+  
+    guard let invite = try Connection.find(id) else {
+      throw Abort.notFound
+    }
+    
+    //Return user as JSON
+    return try invite.makeJSON()
+    }
+  
+  
+    
+}
