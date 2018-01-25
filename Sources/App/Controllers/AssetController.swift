d994c39e56bbf16a7c988f67a178c0e0bd7b1562
Johnny Nguyen <jnguyen1236@gmail.com>
2018-01-25 12:59:43 -0500

    Created basic asset controller for use

---
+++ Sources/App/Controllers/AssetController.swift
@@ -0,0 +1,18 @@
+//
+//  AssetController.swift
+//  App
+//
+//  Created by Johnny Nguyen on 2018-01-25.
+//
+
+import Foundation
+import Vapor
+import HTTP
+
+/**
+ This Asset Controller controls how assets are being handled, There'll be a different service,
+ for actually uploading the required materials, but this is just a handicap of how things will work
+*/
+final class AssetController {
+  
+}
