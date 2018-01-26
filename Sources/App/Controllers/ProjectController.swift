a9671d978f478e286338855ab58de6b9e5dde9ba
Liam Goodwin <liam.goodwin1@stclairconnect.ca>
2018-01-26 13:14:09 -0500

    Added the update, and index functions in the Project controller

--- Sources/App/Controllers/ProjectController.swift
+++ Sources/App/Controllers/ProjectController.swift
@@ -30,5 +30,48 @@ final class ProjectController {
     return try project.makeJSON()
   }
   
+  //MARK: Show all Projects
+  func index(_ reuqest: Request) throws -> ResponseRepresentable {
+    
+    //Return all Projects
+    return try Project.all().makeJSON()
+  }
+  
+  //MARK: Update Project
+  func update(_ request: Request) throws -> ResponseRepresentable {
+    
+    //Declare the project_id requested in the url
+    guard let project_id = request.parameters["id"]?.int
+      else {
+        throw Abort.badRequest
+    }
+    
+    //Declare the project by searching the Project model at the given project_id
+    guard let project = try Project.find(project_id)
+      else {
+        throw Abort.notFound
+    }
+    
+    //Update name, project_description, and description_needs if they have been passed through the url
+    project.name = request.json?["name"]?.string ?? project.name
+    project.project_description = request.json?["project_description"]?.string ?? project.project_description
+    project.description_needs = request.json?["description_needs"]?.string ?? project.description_needs
+    
+    //Update category_id, and role_id if they have been requested to change
+    if let category_id = request.json?["category_id"]?.int {
+      project.category_id = Identifier(category_id)
+    }
+    
+    if let role_id = request.json?["role_id"]?.int {
+      project.role_id = Identifier(role_id)
+    }
+    
+    //Save the project
+    try project.save()
+    
+    //Return the project as JSON
+    return try project.makeJSON()
+  }
+  
 }
 
