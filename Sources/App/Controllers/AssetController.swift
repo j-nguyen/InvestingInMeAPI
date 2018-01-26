8a372397e0b84f0fdecd12b69b9054d409a08fb8
Johnny Nguyen <jnguyen1236@gmail.com>
2018-01-25 20:34:09 -0500

    Updated uploadfile function to start working and started work on controller

--- Sources/App/Controllers/AssetController.swift
+++ Sources/App/Controllers/AssetController.swift
@@ -14,5 +14,15 @@ import HTTP
  for actually uploading the required materials, but this is just a handicap of how things will work
 */
 final class AssetController {
-  
+  /**
+    Attempts to create the asset, by first creating the model, then attempting to upload an image
+  */
+  func create(_ req: Request) throws -> ResponseRepresentable {
+    // im gonna assume this would be ok for base64 content
+    guard let file = req.json?["file"]?.string else {
+      throw Abort.badRequest
+    }
+    
+    return Response(status: .ok)
+  }
 }
--- Sources/App/Services/CloudinaryService.swift
+++ Sources/App/Services/CloudinaryService.swift
@@ -9,12 +9,13 @@ final class CloudinaryService {
   // MARK: Properties
   private let apiKey: String
   private let apiSecret: String
+  private let uploadPreset: String
   
   // Our base url to create
-  private static let baseUrl: String = "https://api.cloudinary.com/v1_1/dvrolb5ai"
+  private let baseUrl: String = "https://api.cloudinary.com/v1_1/dvrolb5ai"
   
   // MARK: Enums
-  enum ContentType {
+  enum ContentType: String {
     case image
     case video
   }
@@ -24,10 +25,12 @@ final class CloudinaryService {
  
     - parameter apiKey: The Api Key Token String for us we need in our url parameters
     - parameter apiSecret: The Secret we need to enter in our headers
+    - parameter uploadPreset: The contents of which the metadata gets stored
   */
-  init(apiKey: String, apiSecret: String) {
+  init(apiKey: String, apiSecret: String, uploadPreset: String) {
     self.apiKey = apiKey
     self.apiSecret = apiSecret
+    self.uploadPreset = uploadPreset
   }
   
   /**
@@ -39,7 +42,8 @@ final class CloudinaryService {
   convenience init(config: Config) throws {
     try self.init(
       apiKey: config.get("apiKey"),
-      apiSecret: config.get("apiSecret")
+      apiSecret: config.get("apiSecret"),
+      uploadPreset: config.get("uploadPreset")
     )
   }
   
@@ -47,9 +51,22 @@ final class CloudinaryService {
     Attempts to upload the file based on the content type chosen
    
     - parameter type: The file type of its used
-    - parameter fileName: The name of the file string
+    - parameter asset: The Asset Database model used to record the results after a successful save
+    - parameter file: A Data URI base64-encoded used to save the image files for us
   */
-  func uploadFile(type: ContentType, fileName: String) {
+  func uploadFile(type: ContentType, asset: Asset, file: String) throws {
+    // this will generate the url
+    let url = "\(baseUrl)/\(type.rawValue)/upload"
     
+    // set up our headers here
+    let headers: [HeaderKey: String] = [.contentType: "application/json"]
+    
+    // set up our body content
+    var json = JSON()
+    try json.set("file", file)
+//    try json.set("fileName", fileName)
+    
+    // set up the request
+//    let request = Request(method: .post, uri: url, body: )
   }
 }
