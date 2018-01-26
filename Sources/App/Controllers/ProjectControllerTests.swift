217cb075f753ee1e6820ff50aa3a1291fa206958
Liam Goodwin <liam.goodwin1@stclairconnect.ca>
2018-01-26 15:19:46 -0500

    Removed the Project Controller test

--- Sources/App/Controllers/ProjectControllerTests.swift
+++ Sources/App/Controllers/ProjectControllerTests.swift
@@ -14,4 +14,25 @@ import Sockets
 
 class ProjectControllerTests: TestCase {
   
+  let controller = ProjectController()
+  
+  func testPostRoutes() throws {
+    guard let projectOne = try createNewProject(), let idOne = projectOne.id?.int else {
+      XCTFail()
+      return
+    }
+  }
+  
+  func createNewProject() throws -> Project? {
+    let request = Request.maketest(method: .post)
+    request.json = try JSON(node: ["user_id": 1, "name": "Test Name", "category_id": 1, "role_id": 1, "project_description": "Test project description", "description_needs": "Test description needs"])
+    let response = try controller.create(req)
+    
+    XCTAssertNotNil(response)
+    let newId: Int? = try response?.get("id")
+    XCTAssertNotNil(newId)
+    XCTAssertNotNil(json?["content"])
+    return try Project.find(newId)
+  }
+  
 }
