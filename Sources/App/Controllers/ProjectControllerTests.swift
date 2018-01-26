58a541b509af789846a02d5b06fdbcabaa69557c
Liam Goodwin <liam.goodwin1@stclairconnect.ca>
2018-01-26 14:18:14 -0500

    Added Project Controller test

---
+++ Sources/App/Controllers/ProjectControllerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectControllerTests.swift
+//  App
+//
+//  Created by Liam Goodwin on 2018-01-26.
+//
+
+import XCTest
+import Testing
+import HTTP
+import Sockets
+@testable import Vapor
+@testable import App
+
+class ProjectControllerTests: TestCase {
+  
+}
