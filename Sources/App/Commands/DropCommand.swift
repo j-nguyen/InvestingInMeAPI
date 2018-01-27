48eb2532e79d50e4639655828d533d2d167ccdff
Johnny Nguyen <jnguyen1236@gmail.com>
2018-01-27 14:04:49 -0500

    Added a drop database so it's easier for us to drop our contents

---
+++ Sources/App/Commands/DropCommand.swift
@@ -0,0 +1,38 @@
+//
+//  DropCommand.swift
+//  App
+//
+//  Created by Johnny Nguyen on 2018-01-27.
+//
+
+import Foundation
+import Vapor
+import Console
+
+/**
+ Helps us drop all the tables on Postgres, and re-creates the database for us
+*/
+final class DropCommand: Command {
+  let id: String = "seed"
+  let help: [String] = ["Helps populate data so that we know what our application is going to look like"]
+  let console: ConsoleProtocol
+  
+  init(console: ConsoleProtocol) {
+    self.console = console
+  }
+  
+  func run(arguments: [String]) throws {
+    try drop?.database?.raw("DROP SCHEMA public CASCADE;")
+    try drop?.database?.raw("CREATE SCHEMA public;")
+    try drop?.database?.raw("GRANT ALL ON SCHEMA public TO postgres;")
+    try drop?.database?.raw("GRANT ALL ON SCHEMA public TO public;")
+  }
+}
+
+extension DropCommand: ConfigInitializable {
+  convenience init(config: Config) throws {
+    let console = try config.resolveConsole()
+    self.init(console: console)
+  }
+  
+}
