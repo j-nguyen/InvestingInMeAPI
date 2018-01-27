a16313ab59483c80cac006247a97b2db1d3e974a
Johnny Nguyen <jnguyen1236@gmail.com>
2018-01-27 01:36:32 -0500

    Remodeled middleware to point to login method now, and set up auth middleware for our session checks

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -8,6 +8,7 @@
 import Foundation
 import Vapor
 import HTTP
+import JWT
 
 final class UserController {
   
@@ -72,6 +73,73 @@ final class UserController {
     
     //Return user as JSON
     return try user.makeJSON()
+  }
+  
+  func login(_ request: Request) throws -> ResponseRepresentable {
+    // check for authorization token in the header
+    guard let token = request.headers["Authorization"]?.string else {
+      throw Abort(.notFound, reason: "Authorization header not found or invalid.")
+    }
+    
+    let jwt = try JWT(token: token)
+    
+    // find the right key to test
+    guard let keys = try GoogleService.getJWTKeys(token: jwt)
+      else {
+        throw Abort.serverError
+    }
+    
+    // verify that our signatures are right
+    guard let _ = try? jwt.verifySignature(using: RS256(rsaKey: RSAKey(n: keys.n, e: keys.e)))
+      else {
+        throw Abort.serverError
+    }
+    
+    // makes sure our clients match
+    guard let aud = jwt.payload["aud"]?.string, let client_id = drop?.config["google", "client_id"]?.string, aud == client_id else
+    {
+      throw Abort(.unauthorized, reason: "Invalid login credentials.")
+    }
+    
+    // make sure the issuer sets up on google
+    guard let iss = jwt.payload["iss"]?.string, iss == "https://accounts.google.com" else {
+      throw Abort(.unauthorized, reason: "Invalid login credentials.")
+    }
+    
+    // check for expiration
+    guard let exp = jwt.payload["exp"]?.int, TimeInterval(exp) > Date().timeIntervalSince1970 else {
+      throw Abort(.unauthorized, reason: "Expired Token, please re-login")
+    }
+    
+    // check for the sub and make sure that there's a user existing
+    guard let sub = jwt.payload["sub"]?.int else {
+      throw Abort(.unprocessableEntity, reason: "Could not find your user data!")
+    }
+    
+    // create a payload for the jwt to add in, this is set to expire in a day
+    var payload = JSON(ExpirationTimeClaim(createTimestamp: { Int(Date().timeIntervalSince1970) + 86400 }))
+    
+    // if it doesn't exist, we'll attempt to make one
+    if let user = try User.makeQuery().filter("google_id", sub).first() {
+      try payload.set("user_id", user.id)
+    } else {
+      let user = User(
+        google_id: sub,
+        email: try jwt.payload.get("email"),
+        name: try jwt.payload.get("name"),
+        picture: try jwt.payload.get("picture"),
+        email_verification: try jwt.payload.get("email_verified")
+      )
+      
+      try user.save()
+      
+      try payload.set("user_id", user.id)
+    }
+    
+    // not the best way to sign for now, but this is for a developmental purpose standpoint.
+    let authToken = try JWT(payload: payload, signer: HS512(key: "login".bytes))
+    
+    return try JSON(node: ["token": authToken])
   }
   
 }
--- Sources/App/Middleware/AuthMiddleware.swift
+++ Sources/App/Middleware/AuthMiddleware.swift
@@ -4,37 +4,13 @@ import HTTP
 import JWT
 import Foundation
 
+/**
+ AuthMiddleware to authenticate users to make sure it's set up
+*/
 final class AuthMiddleware: Middleware {
   func respond(to request: Request, chainingTo next: Responder) throws -> Response {
     
-    guard let token = request.headers["Authorization"]?.string else {
-      throw Abort(.notFound, reason: "Authorization header not found or invalid.")
-    }
-    
-    let jwt = try JWT(token: token)
-    
-    guard let keys = try GoogleService.getJWTKeys(token: jwt)
-      else {
-        throw Abort.serverError
-    }
-    
-   guard let _ = try? jwt.verifySignature(using: RS256(rsaKey: RSAKey(n: keys.n, e: keys.e)))
-    else {
-      throw Abort.serverError
-    }
-    
-    guard let aud = jwt.payload["aud"]?.string, let client_id = drop?.config["google", "client_id"]?.string, aud == client_id else
-    {
-      throw Abort(.unauthorized, reason: "Invalid login credentials.")
-    }
-    
-    guard let iss = jwt.payload["iss"]?.string, iss == "https://accounts.google.com" else {
-      throw Abort(.unauthorized, reason: "Invalid login credentials.")
-    }
-    
-    guard let exp = jwt.payload["exp"]?.int, TimeInterval(exp) > Date().timeIntervalSince1970 else {
-      throw Abort(.unauthorized, reason: "Expired Token, please re-login")
-    }
+
     
     return try next.respond(to: request)
   }
