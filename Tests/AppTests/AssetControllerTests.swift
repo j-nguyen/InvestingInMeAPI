b62e39bcb9ecfb78a72decc868e391aac10f67e7
Johnny Nguyen <jnguyen1236@gmail.com>
2018-02-02 20:43:44 -0500

    Finished adding assets tests

--- Tests/AppTests/AssetControllerTests.swift
+++ Tests/AppTests/AssetControllerTests.swift
@@ -9,6 +9,7 @@ import XCTest
 import Foundation
 import Testing
 import HTTP
+import Random
 @testable import Vapor
 @testable import App
 
@@ -16,16 +17,49 @@ class AssetControllerTests: TestCase {
   
   let drop = try! Droplet.testable()
   
+  var user: User?
+  var project: Project?
+  
   override func setUp() {
     super.setUp()
     Testing.onFail = XCTFail
-    // create some initial setup
-    
+    // create some initial setup for project and user
+    user = try! User(google_id: URandom().makeInt(), email: "fakeuser@example.com", name: "Fake User", picture: "", email_verification: true)
+    try! user!.save()
+    // attempt to create the project next
+    project = try! Project(
+      user_id: user!.assertExists(),
+      name: "Project",
+      category_id: Category.Group.mobileApp.category().assertExists(),
+      role_id: Role.Group.developer.role().assertExists(),
+      project_description: "sdfsdf",
+      description_needs: "sdfspdf"
+    )
+    try! project!.save()
   }
 
   func testCreateAsset() throws {
     // we don't want to use the cloudinary service, so we'll use the manual test in here
-    let asset = Asset(project_id: <#T##Identifier#>, file_type: <#T##String#>, url: <#T##String#>, file_name: <#T##String#>, file_size: <#T##Int64#>, project_icon: <#T##Bool#>, public_id: <#T##String#>)
+    let asset = try Asset(
+      project_id: project!.assertExists(),
+      file_type: "Image",
+      url: "http://via.placeholder.com/1x1",
+      file_name: "placeholder",
+      file_size: 12351,
+      project_icon: false,
+      public_id: "dsopfk"
+    )
     
+    try asset.save()
+    
+    // attempt
+    XCTAssertNotNil(asset)
+  }
+  
+  func testDeleteAssets() throws {
+    // attempt to delete all assets
+    try Asset.makeQuery().delete()
+    
+    XCTAssertEqual(0, try Asset.count())
   }
 }
--- Tests/AppTests/Utilities.swift
+++ Tests/AppTests/Utilities.swift
@@ -11,6 +11,33 @@ extension Droplet {
     try config.setup()
     let drop = try Droplet(config)
     try drop.setup()
+    
+    // Changed it up so that now it'll only delete if the specified values do not exist
+    if try Role.count() == 0 {
+      //Declare the roles we will allow
+      let roles = Role.Group.allValues
+      //Iterate through the list of roles
+      for current_role in roles {
+        //Create a new role based on the current_role
+        let role = Role(role: current_role)
+        //Save the role
+        try role.save()
+      }
+    }
+    
+    // Check it now for Categories
+    if try Category.count() == 0 {
+      //Declare the categories we will allow
+      let categories = Category.Group.allValues
+      
+      //Iterate through the list of categories
+      for current_category in categories {
+        //Create a new category based on the current_category
+        let category = Category(type: current_category)
+        //Save the category
+        try category.save()
+      }
+    }
     return drop
   }
   func serveInBackground() throws {
