36f111e7b4b7f37aeb37a3cf776391a90ce4b693
Johnny Nguyen <jnguyen1236@gmail.com>
2018-02-02 22:49:23 -0500

    Added a reset script to help assist with seeding

---
+++ bin/reset.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Resets the project back to its normal settings
+# This means clearing the database, and resetting the seeds with vapor
+
+echo "Building project..."
+vapor build --verbose
+echo "Done."
+echo "Running Drop Command..."
+vapor run drop --verbose
+echo "Done."
+echo "Running Seed Command..."
+vapor run seed --verbose
+echo "Done."
