4d652c83bedf02b9eddc7668324cc59f73c77873
Johnny Nguyen <jnguyen1236@gmail.com>
2018-02-11 20:28:29 -0500

    Setup the caching for rates

--- Sources/App/Middleware/RateLimitMiddleware.swift
+++ Sources/App/Middleware/RateLimitMiddleware.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Vapor
+import Cache
 import HTTP
 
 /**
@@ -14,8 +15,55 @@ import HTTP
  * This involves using redis for our scenario
 */
 final class RateLimitMiddleware: Middleware {
+  
+  private let cache: CacheProtocol
+  private let limit: Int
+  private let refreshInterval: Double
+  
+  /// Iniitializing our rate limiting
+  init(rate: Rate, cache: CacheProtocol = MemoryCache()) {
+    self.cache = cache
+    self.limit = rate.limit
+    self.refreshInterval = rate.refreshInterval
+  }
+  
   func respond(to request: Request, chainingTo next: Responder) throws -> Response {
     
     return try next.respond(to: request)
+  }
+}
+
+
+/// Rate class to declare our rate limiting service
+public struct Rate {
+  public enum Interval {
+    case second
+    case minute
+    case hour
+    case day
+  }
+
+  // MARK: Properties
+  
+  // the limit is the amount of times before it hits this
+  public let limit: Int
+  public let interval: Interval
+  
+  public init(_ limit: Int, interval: Interval) {
+    self.limit = limit
+    self.interval = interval
+  }
+  
+  public var refreshInterval: Double {
+    switch interval {
+    case .second:
+      return 1
+    case .minute:
+      return 60
+    case .hour:
+      return 3600
+    case .day:
+      return 86400
+    }
   }
 }
