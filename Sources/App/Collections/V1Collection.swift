c38290a9f4aca1e678981a81844aed659dd912e7
Johnny Nguyen <jnguyen1236@gmail.com>
2018-02-11 21:14:13 -0500

    Deleted crypto json in the future to generate our own crypto files

--- Sources/App/Collections/V1Collection.swift
+++ Sources/App/Collections/V1Collection.swift
@@ -6,8 +6,7 @@ import HTTP
  This V1Collection class is to help us organize our routes correctly.
  this route will be specified as `/api/v1/`, as a way to simplify and make our routing
  much easier.
- We conform to RouteCollection so that we can start grouping. EmptyInitializable just means that we can
- add our class without needed to initialize the class
+ We conform to RouteCollection so that we can start grouping.
 */
 final class V1Collection: RouteCollection {
   
