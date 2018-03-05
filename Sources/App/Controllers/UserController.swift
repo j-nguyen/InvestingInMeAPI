814cca435f02bf719a4f8544e86c604c4c2fcae9
Johnny Nguyen <jnguyen1236@gmail.com>
2018-03-04 21:36:17 -0500

    CReate project endpoint now requires an image slideshow followed by if theres a placeholder, it'll upload; otherwise it'll upload a temp icon

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -62,8 +62,8 @@ final class UserController {
     }
     
     // Make sure that we've gathered at least 1 image
-    guard let images = request.json?["images"]?.array, images.count > 0 else {
-      throw Abort(.unprocessableEntity, reason: "We need at least 1 screenshot of the project!")
+    guard let images: [String] = try request.json?.get("images"), images.count > 0 && images.count < 3 else {
+      throw Abort(.unprocessableEntity, reason: "We need at least 1 to 3 screenshots of the project!")
     }
       
     //Instaniate the project using the variables we created
@@ -77,41 +77,46 @@ final class UserController {
     )
     
     // Save the new project
-    if let _ = try? project.save() {
-      // In here, we'll check if the user has an image placed. If not, then it'll create the others for us
-      var projectIconFile = request.json?["projectIcon"]?.string
+    try project.save()
+    
+    // Set up the configurations
+    guard let config = drop?.config["cloudinary"] else { throw Abort.serverError }
+    let cloudService = try CloudinaryService(config: config)
+    
+    // In here, we'll check if the user has an image placed. If not, then it'll create the others for us
+    var projectIconFile = request.json?["projectIcon"]?.string
+    
+    if let projectIconFile = projectIconFile {
       
-      if let projectIconFile = projectIconFile {
-        // Set up the configurations
-        guard let config = drop?.config["cloudinary"] else { throw Abort.serverError }
-        let cloudService = try CloudinaryService(config: config)
-        
-        _ = try cloudService.uploadFile(
-          type: CloudinaryService.ContentType.image,
-          file: projectIconFile,
-          projectIcon: true,
-          project: project
-        )
-      } else {
-        // generate the placeholder like so
-        projectIconFile = project.name.generatePlaceholder()
-        // save the asset
-        let projectIconAsset = try Asset(
-          project_id: project.assertExists(),
-          file_type: "Image",
-          url: projectIconFile ?? "https://via.placeholder.com/100",
-          file_name: "Placeholder",
-          file_size: 0,
-          project_icon: true
-        )
-        try projectIconAsset.save()
-      }
+      _ = try cloudService.uploadFile(
+        type: .image,
+        file: projectIconFile,
+        projectIcon: true,
+        project: project
+      )
+    } else {
+      // generate the placeholder like so
+      projectIconFile = project.name.generatePlaceholder()
+      // save the asset
+      let projectIconAsset = try Asset(
+        project_id: project.assertExists(),
+        file_type: "Image",
+        url: projectIconFile ?? "https://via.placeholder.com/100",
+        file_name: "Placeholder",
+        file_size: 0,
+        project_icon: true
+      )
+      try projectIconAsset.save()
+    }
 
-      // we can now add images into the project
-//      for image in images {
-//        image.get("data")
-//      }
-      
+    // we can now add images into the project
+    for image in images {
+      _ = try cloudService.uploadFile(
+        type: .image,
+        file: image,
+        projectIcon: false,
+        project: project
+      )
     }
     
     //Return the newly created project
