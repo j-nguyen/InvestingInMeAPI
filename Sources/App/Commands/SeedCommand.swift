22d42107d5d461964b12eb8f2cb9448ccbf4d026
Jarrod Maeckeler <jarrodmaeckeler@gmail.com>
2018-03-05 13:06:19 -0500

    Added Seeds

--- Sources/App/Commands/SeedCommand.swift
+++ Sources/App/Commands/SeedCommand.swift
@@ -67,7 +67,7 @@ final class SeedCommand: Command {
             console.print("~~~~ Saved \(projectObject.name) ~~~~~")
         }
       }
-      for i in 1...3 {
+      for a in 1...3 {
         if let project = project  {
           let category = try Category.Group.mobileGameApp.category()
           let role = try Role.Group.marketer.role()
@@ -84,7 +84,7 @@ final class SeedCommand: Command {
           console.print("~~~~ Saved \(projectObject.name) ~~~~~")
         }
       }
-      for i in 1...3 {
+      for b in 1...3 {
         if let project = project  {
           let category = try Category.Group.desktop.category()
           let role = try Role.Group.investor.role()
