adbe23fb4fe7b45f9a48e1d8b87affc30c6efd2d
Johnny Nguyen <jnguyen1236@gmail.com>
2018-03-31 16:12:41 -0400

    Added request queries to sort, and to find read ones only

--- Sources/App/Controllers/NotificationController.swift
+++ Sources/App/Controllers/NotificationController.swift
@@ -16,9 +16,23 @@ final class NotificationController {
       throw Abort.badRequest
     }
     
-    let notifications = try Notification.makeQuery().filter("user_id", userId).all()
+    let notifications = try Notification.makeQuery().filter("user_id", userId)
     
-    return try notifications.makeJSON()
+    // We'll set up search queries to make sure that it sorts, or it only gets read ones
+    if let sort = req.query?["search"]?.string {
+      if sort == "desc" {
+        try notifications.sort("created_at", .descending)
+      } else if sort == "asc" {
+        try notifications.sort("created_at", .ascending)
+      }
+    }
+    
+    // Check now to only show read ones
+    if let read = req.query?["read"]?.bool {
+      try notifications.filter("has_read", read)
+    }
+    
+    return try notifications.all().makeJSON()
   }
   
   func delete(_ req: Request) throws -> ResponseRepresentable {
