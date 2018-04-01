72874535e895ab2f0d2333011f82b249a0656b87
Johnny Nguyen <jnguyen1236@gmail.com>
2018-04-01 00:25:23 -0400

    Set it for optional for now

--- Sources/App/Migrations/AddUserIdToNotification.swift
+++ Sources/App/Migrations/AddUserIdToNotification.swift
@@ -12,7 +12,7 @@ import FluentProvider
 struct AddUserIdToNotification: Preparation {
   static func prepare(_ database: Database) throws {
     try database.modify(Notification.self) { db in
-      db.parent(User.self, foreignIdKey: "owner_id")
+      db.parent(User.self, optional: true, foreignIdKey: "owner_id")
     }
   }
   
