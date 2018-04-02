800d4a834cc65e56f4cce73c17121348d7bb94f7
Jarrod Maeckeler <jarrodmaeckeler@gmail.com>
2018-04-02 14:24:49 -0400

    Fixed phone number validation

--- Sources/App/Validation/OnlyPhoneNumberValidator.swift
+++ Sources/App/Validation/OnlyPhoneNumberValidator.swift
@@ -10,31 +10,23 @@ import Validation
 
 public struct OnlyPhoneNumberValidator: Validator {
   
-  public init() { }
+  private let numeric = "0123456789"
   
-  /**
-   Validate whether or not this is a proper phone number, by testing the count length and location
-   
-   - parameter value: input value to validate
-   
-   - throws: an error if validation fails
-   */
-  public func validate(_ input: String) throws {
-<<<<<<< HEAD
-    let passed = !input
-      .lowercased()
-      .contains { !allowedCharacters.contains($0) }
-
-    try Count.max(10).validate(input)
-
-    guard passed else {
-      throw Abort(.badRequest, reason: "You've entered something naughty! Please try again.")
-    }
-=======
-    guard input.range(of: "^\\d{3}-\\d{3}-\\d{4}$", options: [.regularExpression]) != nil else {
-      throw Abort(.badRequest, reason: "Not a valid phone number!")
+    /// See Validator.inverseMessage
+    public var inverseMessage: String {
+      return "alphanumeric"
     }
     
->>>>>>> staging
-  }
+    /// creates a new alphanumeric validator
+    public init() {}
+    
+    /// See Validator.validate
+    public func validate(_ input: String) throws {
+        for char in input.lowercased() {
+          guard numeric.contains(char) else {
+            throw Abort(.badRequest, reason: "You've entered something naughty! Please try again.")
+          }
+        }
+      }
 }
+
