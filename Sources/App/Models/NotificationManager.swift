aa4f3e49fc6b767aafb36fb175b98dcdee8b48c1
Liam Goodwin <liam.goodwin1@stclairconnect.ca>
2018-04-03 21:02:29 -0400

    Set up the variables and initializers for the NotificationManager model

--- Sources/App/Models/NotificationManager.swift
+++ Sources/App/Models/NotificationManager.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import Vapor
+import FluentProvider
+
+public final class NotificationManager: Model {
+  public var uuid: String
+  public var type: String
+  public var type_id: Int
+  
+  
+  public let storage: Storage = Storage()
+  
+  public init(uuid: String, type: String, type_id: Int) {
+    self.uuid = uuid
+    self.type = type
+    self.type_id = type_id
+  }
+  
+  public init(row: Row) {
+    
+  }
+}
