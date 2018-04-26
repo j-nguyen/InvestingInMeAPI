91c8e934d5df1e2f18f46f6ff2b5e028e7d443bd
Johnny Nguyen <jnguyen1236@gmail.com>
2018-04-25 21:57:39 -0400

    Added swiftrandom to generate seed data

--- Package.swift
+++ Package.swift
@@ -16,11 +16,12 @@ let package = Package(
         .package(url: "https://github.com/vapor/redis-provider.git", .upToNextMajor(from: "2.0.1")),
         .package(url: "https://github.com/vapor/validation.git", .upToNextMajor(from: "1.1.1")),
         .package(url: "https://github.com/vapor/leaf-provider.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/iamjono/SwiftRandom.git", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
         .target(
             name: "App",
-            dependencies: ["Vapor", "FluentProvider", "PostgreSQLProvider", "JWT", "RedisProvider", "Validation", "LeafProvider"],
+            dependencies: ["Vapor", "FluentProvider", "PostgreSQLProvider", "JWT", "RedisProvider", "Validation", "LeafProvider", "SwiftRandom"],
             exclude: ["Config", "Public", "Resources"]
         ),
         .target(name: "Run", dependencies: ["App"]),
